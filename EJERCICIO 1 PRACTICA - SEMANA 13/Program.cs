55a4170541b44660275241842869881dc4428c4e
katty0306 <kattymm635@gmail.com>
2024-11-07 10:52:40 -0300

    PRIMER COMMIT - MENU

--- EJERCICIO 1 PRACTICA - SEMANA 13/Program.cs
+++ EJERCICIO 1 PRACTICA - SEMANA 13/Program.cs
@@ -1,7 +1,81 @@
-﻿using EJERCICIO_1_PRACTICA___SEMANA_13;
+﻿
+byte opcion;
+
+string regresar;
+
+do
+{
+
+    Console.WriteLine("DESAYUNOS DEL MES\n");
+
+    Console.WriteLine("******* MENÚ *******");
+
+    Console.WriteLine("* 1. Crear   *");
+
+    Console.WriteLine("* 2. Listar  *");
+
+    Console.WriteLine("* 3. Eliminar *");
+
+    Console.WriteLine("* 0. Fin      *");
+
+    Console.WriteLine("********************\n");
 
 
-Desayuno desayuno = new Desayuno();
-desayuno.Nombre = "dgfj";
 
-Console.WriteLine(desayuno);
+    Console.Write("Ingrese opcion: ");
+
+
+
+    while (!byte.TryParse(Console.ReadLine(), out opcion) || opcion > 4)
+    {
+
+        Console.Write("Error: Ingrese opcion: ");
+
+    }
+
+
+
+    switch (opcion)
+    {
+
+        case 0: Environment.Exit(0); break;
+
+        case 1: break;
+
+        case 2: break;
+
+        case 3: break;
+
+
+    }
+
+    Console.WriteLine("Desea regresar al menú? [si]: ");
+
+    regresar = Console.ReadLine();
+
+    Console.Clear();
+
+} while (regresar == "si");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
